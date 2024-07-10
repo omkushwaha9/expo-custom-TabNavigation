549e0cc51c3981fd25ee9cb82d43e081775199d1
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-10 13:13:24 +0530

    TabNavigation

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,41 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import TabBar from '../components/TabBar'
 
 const _layout = () => {
   return (
-    <Tabs />
+    <Tabs
+       tabBar ={props => <TabBar {...props}/>}
+       
+    >
+
+        <Tabs.Screen
+        name='index'
+        options={{
+            title: "Home"
+        }}
+        />
+        <Tabs.Screen
+        name='explore'
+        options={{
+            title: "Explore"
+        }}
+        />
+        <Tabs.Screen
+        name='create'
+        options={{
+            title: "Create"
+        }}
+        />
+        <Tabs.Screen
+        name='profile'
+        options={{
+            title: "Profile"
+        }}
+        />
+        
+    </Tabs>
   )
 }
 
