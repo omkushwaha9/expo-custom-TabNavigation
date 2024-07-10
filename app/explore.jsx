40a871bd83b23a94dcf065b39382f15f97615193
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-10 12:18:44 +0530

    TabNavigation

---
+++ app/explore.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ColorList from '../components/ColorList'
+
+const Explore = () => {
+  return (
+    <View>
+      <ColorList color="#4f46e5" />
+    </View>
+  )
+}
+
+export default Explore
